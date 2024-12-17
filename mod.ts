6ee1dd558415e7f166b6bcaadbb17a61eb666c2c
Erwin Heba <erwin.heba@playbookengineering.com>
2024-12-17 17:51:43 +0100

    check logs

--- mod.ts
+++ mod.ts
@@ -1,6 +1,9 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
 const handler = async (req: Request): Promise<Response> => {
+
+  console.log('serwer logs: serwer run!');
+
   return new Response("Hello from Deno2!", {
     headers: { "content-type": "text/plain" },
   });
